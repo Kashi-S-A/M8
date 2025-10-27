1f440bc74916aec8cbd631f7071c9ec02ce372d5
Kashi-S-A <kashappaappannavar@gmail.com>
2025-10-27 12:49:05 +0530

    Uni-D-Mapping

---
+++ 03-HB-One-To-Many-Uni/src/main/java/com/ty/entity/Category.java
@@ -0,0 +1,43 @@
+package com.ty.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Category {
+
+	@Id
+	private int id;
+	private String name;
+
+	@OneToMany
+	private List<Product> produts;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public List<Product> getProduts() {
+		return produts;
+	}
+
+	public void setProduts(List<Product> produts) {
+		this.produts = produts;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
